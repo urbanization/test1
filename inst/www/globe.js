f1d74110e341391961e8889f682b30e4dbc8dd7c
urbanization <zhouyuyu@gmail.com>
2014-08-16 12:01:47 -0400

    Update globe.js

--- inst/www/globe.js
+++ inst/www/globe.js
@@ -78,7 +78,7 @@ DAT.Globe = function(container, colorFn) {
 
   var mouse = { x: 0, y: 0 }, mouseOnDown = { x: 0, y: 0 };
   var rotation = { x: 0, y: 0 },
-      target = { x: Math.PI*3/2, y: Math.PI / 6.0 },
+      target = { x: 0, y: 0 },
       targetOnDown = { x: 0, y: 0 };
 
   var distance = 800, distanceTarget = 800;
