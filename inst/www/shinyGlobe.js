c7eda00005bc27344dd2cd383f057d6fedf6cc77
urbanization <zhouyuyu@gmail.com>
2014-08-17 16:44:58 -0400

    Update shinyGlobe.js

--- inst/www/shinyGlobe.js
+++ inst/www/shinyGlobe.js
@@ -18,6 +18,13 @@
         // Create from scratch
         var container = document.getElementById(el.id);
         var globe = new DAT.Globe(container);
+        
+        
+      var settime = function(globe, t) {
+         return function() {
+          new TWEEN.Tween(globe).to({time: t/5},500).easing(TWEEN.Easing.Cubic.EaseOut).start();
+      };
+      };
   
         var i, tweens = [];
         
@@ -26,13 +33,17 @@
         var data = JSON.parse(data);
         $(el).data('globedata', data);
         for (i=0;i<data.length;i++) {
-          globe.addData(data[1], {format: 'magnitude', name: data[0], animated: true});
+           globe.addData(data[i][1], {format: 'magnitude', name: data[i][0], animated: true});
+          //globe.addData(data[1], {format: 'magnitude', name: data[0], animated: true});
         }
         globe.createPoints();
-        globe.time = 0;
+        //globe.time = 0;
+        settime(globe,5)();
         globe.animate();
         document.body.style.backgroundImage = 'none'; // remove loading
       }
+      
+  
     }
   });
   Shiny.outputBindings.register(globeOutputBinding, 'shinyGlobe.globebinding');
